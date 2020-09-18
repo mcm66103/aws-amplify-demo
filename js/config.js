55cce4f2f774239b9865b1bbe47a448b2a6aa160
Maverick Moore <me@maverickmoore.com>
2020-09-18 11:55:42 -0400

    cognito pool js configuration

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_SY9GLgu0U',
+        userPoolClientId: '3tj79p03kmckf2in96bv20ts8j',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
